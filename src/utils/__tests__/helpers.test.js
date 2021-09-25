f1458ea4070ec164d97399002bf95f01aaa0157c
adeola2310 <adekoyejoadeola@gmail.com>
2021-09-25 22:15:54 +0100

    test:added tests folder for components and utils

---
+++ src/utils/__tests__/helpers.test.js
@@ -0,0 +1,6 @@
+// import { timeConverterAMPM } from '../helpers';
+//
+// test('checks if time is in AM OR PM', () => {
+//     const result = timeConverterAMPM('12:00PM');
+//     expect(result).toBeDefined();
+// });
